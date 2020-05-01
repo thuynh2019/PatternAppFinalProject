7fd46539fc32991722e8b8688f5403d8d5dad7dd
thuynh2019 <ty.huynh@smail.astate.edu>
2020-04-30 23:43:54 -0500

    initial

---
+++ PatternAppFinalProject/PatternAppFinalProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PatternAppFinalProject
+//
+//  Created by Ty Huynh on 4/30/20.
+//  Copyright © 2020 Ty Huynh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
